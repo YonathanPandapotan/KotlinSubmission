0a8c59299e6ab448b1c166e93c6f46dd340e7610
Yonathan <yonathan_naibaho@yahoo.com>
2019-09-07 20:27:01 +0700

    Added Profile

---
+++ app/src/main/java/com/example/kotlinsubmission/ActivityMyProfile.kt
@@ -0,0 +1,12 @@
+package com.example.kotlinsubmission
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityMyProfile : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_profile)
+    }
+}
--- app/src/main/java/com/example/kotlinsubmission/MainActivity.kt
+++ app/src/main/java/com/example/kotlinsubmission/MainActivity.kt
@@ -3,8 +3,9 @@ package com.example.kotlinsubmission
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
+import android.view.Menu
 import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -12,15 +13,25 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var item : RecyclerView
     private var list : ArrayList<Item> = arrayListOf()
+    private lateinit var toolbar : Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
         item = findViewById(R.id.recycler)
 
         list.addAll(ItemData.data)
         showRecyclerList()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 
     fun showRecyclerList(){
