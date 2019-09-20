8ce7af635e69f5da257a0d98411e9820c67d80cd
Yonathan <yonathan_naibaho@yahoo.com>
2019-09-20 17:58:33 +0700

    Wrong data was set

--- app/src/main/java/com/example/kotlinsubmission/ItemAdapter.kt
+++ app/src/main/java/com/example/kotlinsubmission/ItemAdapter.kt
@@ -3,10 +3,8 @@ package com.example.kotlinsubmission
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class ItemAdapter(val listItem: ArrayList<Item>) : RecyclerView.Adapter<ItemAdapter.ListViewHolder>() {
